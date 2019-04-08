f0b372b93d6eb0a4cf982a119192ba8bbfe8dedc
Laferu <laferu@users.noreply.github.com>
2019-04-08 01:11:05 -0300

    Arquivo que ficou solto

--- src/App.js
+++ src/App.js
@@ -1,69 +1,55 @@
-import React, { Component, useState } from 'react';
-var Tesseract = window.Tesseract
-var translate = require('yandex-translate')('SUA YANDEX API AQUI');
+import React, { Component, useState, useEffect } from 'react';
+const fs = require('fs');
+const Tesseract = window.Tesseract
+const translate = require('yandex-translate')('SUA YANDEX API');
 
-
-/*
-  let img = new Image()
-  var resote = "aaa"
-  img.src = './images/teste.png'
-  img.onload = function(){
-
-    Tesseract.recognize(img, {
-      lang: 'jpn'
-    }).progress((progress) => {
-      console.log(progress, '$$$$')
-
-      if(progress.status === 'recognizing.text'){
-        console.log(`${progress.progress*100}%`)
-      }
-    }).then((result) => {
-      console.log(result, '$$$$')
-      resote = result.text
-    })
-  }
-*/
-
-
+let dataURI, video, btn, canvas, ctx
 
 export default function app(){
   const [resultado, setResultado] = useState(0)
   
-
-  //EXTRAINDO TEXTO DA IMAGEM
-  let img = new Image()
-  img.src = './images/teste.png'
-  img.onload = function(){
-
-    Tesseract.recognize(img, {
-      lang: 'jpn'
-    }).progress((progress) => {
-      console.log(progress, '$$$$')
-
-      if(progress.status === 'recognizing.text'){
-        console.log(`${progress.progress*100}%`)
+  useEffect(() => {
+    video = document.querySelector("#video")
+    video = document.querySelector("#video")
+    btn = document.querySelector('#btn')
+    navigator.mediaDevices.getUserMedia({
+      video:{
+          mediaSource: "window"
       }
-    }).then((result) => {
-      console.log(result, '$$$$')
-      translate.translate(result.text, { to: 'pt' }, function(err, res) {
-        setResultado(res.text)
-      })
-      
+    }).then(strm => {
+      video.srcObject = strm;
+      video.play();
     })
-  }
 
+    btn.addEventListener('click', async () => {
+      canvas = document.createElement('canvas')
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight
+      ctx = canvas.getContext('2d')
+      ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
+      dataURI = canvas.toDataURL('image/jpeg')
+      document.querySelector("#base_img").value = dataURI
+      
 
+      Tesseract.recognize(dataURI, {
+        lang: 'eng'
+      }).then((result) => {
+        console.log(result, '$$$$')
+        translate.translate(result.text, { to: 'pt' }, function(err, res) {
+          console.log(res.text)
+          setResultado(res.text)
+        })
+      })
+    })
+  })
 
-
-  /*
-  setTimeout(function(){
-    setCount(count + 1)
-  }, 1000)
-  console.log(count)
-  */
   return (
     <div>
       <p>{resultado}</p>
+      <video id="video"></video>
+      <input  type="text" id="base_img" name="base_img"/>
+			<button type="button" id="btn">Tirar foto e salvar</button>
     </div>
   )
+
 }
